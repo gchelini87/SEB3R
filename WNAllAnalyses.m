a4cd1136a5b39665164df7af9e125a8f7e88645c
Enrico "erolm_a" Trombetta <trombetta.enricom@protonmail.com>
2023-04-05 16:32:23 +0200

    Made body part, frame and subject count flexible

--- WNAllAnalyses.m
+++ WNAllAnalyses.m
@@ -1,5 +1,9 @@
-Pathdir = ['WN results'];
-files = dir(Pathdir);
+% This entry point will perform all analyses on your mice data.
+% You will be prompted to provide a folder that contains your DLC results
+% (we default to 'WN results' as a good reference for you).
+
+pathdir = uigetdir();
+files = dir(pathdir);
 nfiles = size(files);
 disp(size(nfiles));
 
@@ -7,7 +11,7 @@ for i=1:nfiles[1]
     if files(i).isdir
         num = files(i).name;
         if ~isnan(str2double(num))
-            WNposeExtractionOneMouse3D(num);
+            WNposeExtractionOneMouse3D(pathdir, num);
         end
     end
 end
